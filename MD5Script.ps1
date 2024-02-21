3e20e4f651302dd14e6953edcafabe3fc68cf5df
Aussie7004 <88077215+Aussie7004@users.noreply.github.com>
2024-02-21 21:13:22 +1100

    Update MD5Script.ps1

--- MD5Script.ps1
+++ MD5Script.ps1
@@ -1,17 +1,19 @@
 function GetHash {
+#Neutralises variable for check without error
     $FilePath = ""
     $FilePath = Read-Host "Please enter a filepath: "
 
     if (!$FilePath) {
-        <# Action when this condition is true #>
+
         Write-Output "`n`nPath cannot be empty.`n`n"
         GetHash
     }
 
+    #Validates filepath
     $Validity = Test-Path -Path $FilePath -PathType Leaf
     $Md5Value = ""
     
-
+    #Verifies path
     if ($Validity -eq $true) {
         $Md5Value = Get-FileHash $FilePath -Algorithm MD5
         Write-Output "`n`n" + $Md5Value + "`n`n"
@@ -40,9 +42,11 @@ function main() {
 
     $Today
 
+    #infinite loop for efficiency 
     while ($true) {
         GetHash
-        function pause { $null = Read-Host 'Press Enter to continue or Ctrl+C to quit' } #creates function and edits default pause message and waits for host to decide what they want to do instead of "enter to continue"
+        #creates function and edits default pause message and waits for host to decide what they want to do instead of "enter to continue"
+        function pause { $null = Read-Host 'Press Enter to continue or Ctrl+C to quit' } 
         Pause
     }
 }
